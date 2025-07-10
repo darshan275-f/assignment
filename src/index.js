aa658ca4aac97a1cb663331070ff701fd5698717
darshan275-f <darshanjomalingpatil@gmail.com>
2025-07-10 18:41:28 +0530

    Refactor server initialization and update package.json scripts

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config({
     path:'./.env'
 })
-
-app.listen(process.env.PORT,()=>{
+const PORT=process.env.PORT || 4000;
+app.listen(PORT,()=>{
     console.log("App is running on ",process.env.PORT)
 })
 
